10cde1e7f9bf511bbb18a5078c7ad17d8ecc445e
leeosilvp <leo965679@gmail.com>
2026-05-26 17:18:11 -0300

    feat: adicionando parceiros na Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,6 +4,13 @@ import { useDate } from '../hooks/useDate'
 import { Link } from "react-router-dom"
 import { ArrowRight } from '@geist-ui/icons'
 
+import partnership1 from '../assets/img/partnership/1.png'
+import partnership2 from '../assets/img/partnership/2.png'
+import partnership3 from '../assets/img/partnership/3.png'
+import partnership4 from '../assets/img/partnership/4.png'
+import partnership5 from '../assets/img/partnership/5.png'
+import partnership6 from '../assets/img/partnership/6.png'
+
 const Home = () => {
 
   const { day, month, year } = useDate()
@@ -15,8 +22,19 @@ const Home = () => {
         <section className="home-presentation">
           <div>
             <p>{month} {day}, {year}</p>
-            <h1>Construindo a <span>Rede</span> de <span>Inteligência Planetária</span>.</h1>
-            <Link>Solicitar Acesso <ArrowRight className='arrow-right' size={16}/></Link>
+            <h1>Rede de Inteligência Planetária.</h1>
+            <Link>Solicitar Acesso <ArrowRight className='arrow-right' size={16} /></Link>
+          </div>
+        </section>
+
+        <section className='home-partnership'>
+          <div className='home-partnership-grid'>
+            <img src={partnership1} />
+            <img src={partnership2} />
+            <img src={partnership3} />
+            <img src={partnership4} />
+            <img src={partnership5} />
+            <img src={partnership6} />
           </div>
         </section>
       </div>
